f7d5995eddce191c5432ff1db5cebc78433c8591
Ninetails <jose_rafael93@hotmail.com>
2018-04-14 05:45:03 -0400

    Update
    Casi lista

--- myScript.js
+++ myScript.js
@@ -1,84 +1,12 @@
-// // smooth scrolling
+// Populate Modal
 
-// $(document).ready(function(){
-//   // Add smooth scrolling to all links
-//   $("a").on('click', function(event) {
-
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== "") {
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
-
-//       // Store hash
-//       var hash = this.hash;
-
-//       // Using jQuery's animate() method to add smooth page scroll
-//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top
-//       }, 800, function(){
-   
-//         // Add hash (#) to URL when done scrolling (default click behavior)
-//         window.location.hash = hash;
-//       });
-//     } // End if
-//   });
-// });
-
-// //sticky back to top
-
-// jQuery(document).ready(function() {
- 
-// var offset = 250;
- 
-// var duration = 300;
- 
-// jQuery(window).scroll(function() {
- 
-// if (jQuery(this).scrollTop() > offset) {
- 
-// jQuery('.back-to-top').fadeIn(duration);
- 
-// } else {
- 
-// jQuery('.back-to-top').fadeOut(duration);
- 
-// }
- 
-// });
- 
- 
- 
-// jQuery('.back-to-top').click(function(event) {
- 
-// event.preventDefault();
- 
-// jQuery('html, body').animate({scrollTop: 0}, duration);
- 
-// return false;
- 
-// })
- 
-// });
-
-// // random hover color
-// $(document).ready(function()
-// {
-//     $(".randhover").hover(function(e)
-//     {
-//         var randomClass = getRandomClass();
-//         $(e.target).attr("class", randomClass);
-//     });
-// });
-
-// function getRandomClass()
-// {
-//     //Store available css classes
-//     var classes = new Array("green", "purple", "teal", "violet", "pink");
-    
-//     //Give a random number from 0 to 5
-//     var randomNumber = Math.floor(Math.random()*6);
-    
-//     return classes[randomNumber];
-// }
-
+$('#servModalCenter').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var servTitle = button.data('servtitle')
+  var servDescription = button.data('servdescription') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('Acerca de nuestro ' + servTitle)
+  modal.find('.modal-body').text(servDescription)
+})
